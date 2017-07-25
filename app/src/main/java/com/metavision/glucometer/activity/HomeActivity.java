0fc7f4a066c0cd39c34ce3ad8d8a60ee32e89fa5
Jafar Alim Habibi <jafarhabib77@gmail.com>
2017-07-26 05:38:13 +0700

    Nothing gonna change. Even though, comments are untrivial

--- app/src/main/java/com/metavision/glucometer/activity/HomeActivity.java
+++ app/src/main/java/com/metavision/glucometer/activity/HomeActivity.java
@@ -2,13 +2,17 @@ package com.metavision.glucometer.activity;
 
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.IdRes;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.metavision.glucometer.R;
@@ -18,6 +22,21 @@ import com.roughike.bottombar.OnTabSelectListener;
 public class HomeActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
+   /* private NavigationView navigationView;
+    private DrawerLayout drawerLayout;
+    private View navHeader;
+    private TextView txtName;
+    private ImageView imgNavHeaderBg, imgProfile;*/
+    private Toolbar toolbar;
+
+    /*public static int navItemIndex = 0;
+
+    private static final String TAG_SETTINGS = "settings";
+
+    private String[] activityTitles;
+
+    private boolean shouldLoadHomeActivityOnBackPress = true;
+    private Handler mHandler;*/
 
 
     /*private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
@@ -51,8 +70,25 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
 
        /* mTextMessage = (TextView) findViewById(R.id.message);*/
-        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
-        setSupportActionBar(myToolbar);
+        toolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
+
+       /* mHandler = new Handler();
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
+
+        navHeader = navigationView.getHeaderView(0);
+        txtName = (TextView) findViewById(R.id.name);
+        imgNavHeaderBg = (ImageView) findViewById(R.id.img_header_bg);
+        imgProfile = (ImageView) findViewById(R.id.img_profile);
+
+        activityTitles = getResources().getStringArray(R.array.next_app);
+
+        loadNavHeader();*/
+
+
+
 //        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         mTextMessage = (TextView) findViewById(R.id.textView);
         BottomBar bottomBar = (BottomBar) findViewById(R.id.navigation);
@@ -108,4 +144,8 @@ public class HomeActivity extends AppCompatActivity {
 //        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
+    /*private void loadNavHeader() {
+        txtName.setText("Gluco user");
+
+    }*/
 }
